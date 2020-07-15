c49abe8558012d71acb744c4f8d9205947bf1205
Aditya-Bhardwaj-99 <bhardwaj.aditya99@gmail.com>
2020-07-15 15:57:28 +0530

    first alpha

--- src/Opo.js
+++ src/Opo.js
@@ -1,14 +1,13 @@
 import React, {Component} from 'react';
-import { TweenLite,TimelineLite } from 'gsap'
 /* get number in props */
 
 export default class Opo extends Component{
     render(){
         return(
             <div>
-                {Array(this.props.cardnum).fill().map(i => {
+                {Array(this.props.cardnum).fill().map((_,i) => {
               return (
-                <img src={require("./cards/back-.png")} className='op3cards' ref={this.props.name+i} alt='back'
+                <img src={require("./cards/back-.png")} className={this.props.name+'cards'} id={this.props.name+i} alt='back'
                 style={
                     i === 0
                       ? { zIndex: `${i}`, marginLeft: "0 !important",background:'black',height:'75px',width:'50px' }
